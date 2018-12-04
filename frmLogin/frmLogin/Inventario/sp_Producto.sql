96313ac7489c504269d4635ac64ddc3447e9f1ee
marcoselisorto <marcoselisorto@hotmail.com>
2018-12-03 22:11:25 -0600

    Creación de la clase producto y el archivo sql para los procedimientos almacenados de la tabla

---
+++ frmLogin/frmLogin/Inventario/sp_Producto.sql
@@ -0,0 +1,55 @@
+﻿/*Contiene los procedimientos almacenados de
+iserción
+actualización
+inhabilitacion
+de la tabla inventario.Producto
+*/
+
+CREATE PROCEDURE sp_InsertarProducto(
+@idInventario int,
+@idProducto int,
+@nombre NVARCHAR(100),
+@cantidadExistencia INT,
+@cantidadMinima INT,
+@precioCompra FLOAT,
+@precioVenta FLOAT,
+@fechaIngreso DATETIME,
+@idUsuario INT,
+@observaciones NVARCHAR(100),
+@idImpuesto INT,
+@idCategoria INT,
+@idProveedor INT,
+@estado Bit
+)
+AS
+BEGIN
+	INSERT INTO Inventario.Producto(idProducto,nombre,cantidadExistencia,cantidadMinima,precioCompra,precioVenta,fechaIngreso,
+								    idUsuario,observaciones,idImpuesto,idCategoria,idProveedor,estado)
+VALUES(@idProducto,@nombre,@cantidadExistencia,@cantidadMinima,@precioCompra,@precioVenta,@fechaIngreso,@idUsuario,@observaciones,
+	   @idImpuesto,@idCategoria,@idProveedor,@estado);
+END
+GO
+
+CREATE PROCEDURE sp_ActualizarProducto(
+@idInventario int,
+@idProducto int,
+@nombre NVARCHAR(100),
+@cantidadExistencia INT,
+@cantidadMinima INT,
+@precioCompra FLOAT,
+@precioVenta FLOAT,
+@fechaIngreso DATETIME,
+@idUsuario INT,
+@observaciones NVARCHAR(100),
+@idImpuesto INT,
+@idCategoria INT,
+@idProveedor INT,
+@estado Bit
+)
+AS
+BEGIN
+	INSERT INTO Inventario.Producto(idProducto,nombre,cantidadExistencia,cantidadMinima,precioCompra,precioVenta,fechaIngreso,
+								    idUsuario,observaciones,idImpuesto,idCategoria,idProveedor,estado)
+VALUES(@idProducto,@nombre,@cantidadExistencia,@cantidadMinima,@precioCompra,@precioVenta,@fechaIngreso,@idUsuario,@observaciones,
+	   @idImpuesto,@idCategoria,@idProveedor,@estado);
+END
