b5f5776e0d983aefc3a4a81ca977ac791e167601
PENA98 <maynorx5000@gmail.com>
2018-12-03 22:16:37 -0600

    Crear clase y formulario de usuarios

---
+++ frmLogin/frmLogin/Empleados/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frmLogin.Empleados
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public int idEmpleado { get; set; }
+        public string nombreUsuario { get; set; }
+        public string contrasena { get; set; }
+
+
+    }
+}
