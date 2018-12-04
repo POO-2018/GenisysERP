361a6efe3654764e21dce9de12532961b93a4cbd
Sonia Villeda <sonia08_m@hotmail.com>
2018-12-03 23:26:57 -0600

    Creación de Clases Compras y  Detalle Compra, y propiedades.

---
+++ frmLogin/frmLogin/Compras/Compra.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Agregamos los namespace necesario
+using System.Data;
+using System.Data.SqlClient;
+
+namespace frmLogin.Compras
+{
+    class Compra
+    {
+        // Propiedades
+        public int idCompra { get; set; }
+        public DateTime fechaCompra { get; set; }
+        public string numeroFactura { get; set; }
+        public int idProveedor { get; set; }
+        public string nombreProveedor { get; set; }
+        public decimal subTotal { get; set; }
+        public decimal impuesto { get; set; }
+        public decimal total { get; set; }
+        public string observaciones { get; set; }
+        public string estadoCompra { get; set; }
+        public int idUsuario { get; set; }
+        public string nombreUsuario { get; set; }
+        public string autorizadaPor { get; set; }
+        public int estado { get; set; }
+    }
+}
---
+++ frmLogin/frmLogin/Compras/DetalleCompra.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Agregamos los namespace necesario
+using System.Data;
+using System.Data.SqlClient;
+
+namespace frmLogin.Compras
+{
+    class DetalleCompra
+    {
+        // Propiedades
+
+        public int idDetalle { get; set; }
+        public int idCompra { get; set; }
+        public int idProducto { get; set; }
+        public string nombreProducto { get; set; }
+        public decimal precioUnitario { get; set; }
+        public int cantidad { get; set; }
+        public decimal subTotal { get; set; }
+
+
+
+    }
+}
