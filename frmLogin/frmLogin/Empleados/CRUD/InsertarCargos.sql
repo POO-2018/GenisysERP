9f48d009bf819d71aaa69f88ec8456441c9fc168
PENA98 <maynorx5000@gmail.com>
2018-12-13 05:08:26 -0600

    Insert statements

--- frmLogin/frmLogin/Empleados/CRUD/InsertarCargos.sql
+++ frmLogin/frmLogin/Empleados/CRUD/InsertarCargos.sql
@@ -30,3 +30,33 @@ INSERT INTO [Empleados].[Cargo]
            ('Contador'
            ,1)
 GO
+
+INSERT INTO [Empleados].[Empleado]
+           ([identidad]
+           ,[nombre]
+           ,[apellido]
+           ,[cargo]
+           ,[telefono]
+           ,[direccion]
+           ,[correo]
+           ,[estado])
+     VALUES
+           ('0318-1995-00191'
+           ,'Carlos'
+           ,'Valladarez'
+           ,1
+           ,'9856-4512'
+           ,'Comayagua'
+           ,'Valladares@gmail.com'
+           ,1)
+GO
+
+INSERT INTO [Empleados].[Usuario]
+           ([idEmpleado]
+           ,[nombreUsuario]
+           ,[contrasena])
+     VALUES
+           (1
+           ,'Admin'
+           ,'5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5')
+GO
